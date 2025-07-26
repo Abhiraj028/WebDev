c0283c79a6f088c41c9ea7c2e3266d07b2d5ddf0
Abhiraj028 <abhirajgautam028@gmail.com>
2025-07-26 23:48:28 +0530

    Added auto joke set (array.map)

--- ScrimbaReact/Joke/src/App.jsx
+++ ScrimbaReact/Joke/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Joke from '../components/Joke'
+import jokesData from '../data/joke.js'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const newArr = jokesData.map(joke => {
+    return <Joke setup={joke.setup} punchline={joke.punchline}/>
+  })
 
   return (
-    <div className="App">
-      <Joke setup="Why don't scientists trust atoms?" punchline="Because they make up everything!" />
-      <Joke setup="What do you call fake spaghetti?" punchline="An impasta!" />
-      <Joke setup="Why did the scarecrow win an award?" punchline="Because he was outstanding in his field!" />
-      <Joke setup="Barcelona or Real Madrid?" punchline="Neither, I prefer watching paint dry!" />
-      <Joke punchline="I told my computer I needed a break, and now it won't stop sending me beach wallpapers!" />
-    </div>
+    <>
+    {newArr}
+    </>
   )
 }
 
