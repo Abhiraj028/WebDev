bfc7e08d8800c023ad019412bdb9fc40a6ee5889
Abhiraj028 <abhirajgautam028@gmail.com>
2025-07-26 18:38:44 +0530

    First div entry travelJournal

---
+++ ScrimbaReact/travelJournal/components/Entry.jsx
@@ -0,0 +1,25 @@
+export default function Entry(){
+    return(
+        <div className="entry">
+            <div className="box">
+                <div className="picture">
+                    <img className="img-japan" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Japan" />
+                </div>
+                <div className="rhs-text">
+                    <div className="mapsStuff">
+                        <div className="mapsRHS">
+                            <img className="icon" src="https://png.pngtree.com/png-clipart/20230924/original/pngtree-vector-eps-10-of-a-red-map-marker-icon-isolated-on-png-image_12670278.png" alt="Map Icon" />
+                            <span className="location">J A P A N</span>
+                        </div>
+                        <a href="https://gohttps://www.google.com/maps/place/Japan" target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+                    </div>
+                    <h1 className="mapName">Mount Fuji</h1>
+                    <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
+                    <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                </div>
+            </div>
+            <div class="divider"></div>
+                
+        </div>
+    )
+}
